7a695ce87dd2e33a03a2dcc3010f81ae1007570f
Neycourt5 <Acourtney1990@gmail.com>
2021-09-16 22:59:22 +0900

    Made it display the day of week and high low temps

--- src/components/Weathercards.js
+++ src/components/Weathercards.js
@@ -2,13 +2,19 @@ import React from 'react'
 import Weathercard from './Weathercard'
 
 const Weathercards = ({ forecast, isLoading }) => {
+    const getDayOfWeek = (date) => {
+        const dayOfWeek = new Date(date).getDay();
+        return isNaN(dayOfWeek) ? null :
+            ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+    }
+
     return isLoading ? <div className='weather-cards-group'
     >Loading...</div> :
         <div className='weather-cards-group'>
             {forecast.map((day) => <Weathercard
-                day={day.date}
+                day={getDayOfWeek(day.date)}
                 icon={day.day.condition.icon}
-                temp={`${day.day.avgtemp_c}°C`}
+                temp={`${Math.round(day.day.maxtemp_c)}°/${Math.round(day.day.mintemp_c)}°`}
                 key={day.date_epoch}
             />
             )}
